d6ff6428eaefc62dd664a335d697ea867327b8fc
Michael <michael.sergeew@gmail.com>
2021-01-03 11:06:53 +0300

    Initial Commit

---
+++ avito1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avito1
+//
+//  Created by Mikhail Sergeev on 03.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
